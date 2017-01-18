996af71da22b46e1a6d7b9877e05f104ffbcbf35
Daniel <Daniel.franzen.blubb@gmail.com>
2017-01-18 14:52:01 +0100

    bug fixes

--- www/fileStorage.js
+++ www/fileStorage.js
@@ -1,21 +1,17 @@
-window.cordova.define("com.dfranzen.cordova.FileStorage.FileStorage", function (require, exports, module) {
-    'use strict';
-
-    module.exports = {
-        open: function (success, failure) {
-            window.cordova.exec(success, failure, "FileChooser", "open", []);
+module.exports = {
+    open: function (success, failure) {
+        window.cordova.exec(success, failure, "FileStorage", "open", []);
+    },
+    openPath: function (success, failure) {
+        window.cordova.exec(success, failure, "FileStorage", "open_path", []);
         },
-        openPath: function (success, failure) {
-            window.cordova.exec(success, failure, "FileChooser", "open_path", []);
-        },
-        readFromUri: function (success, failure, uri) {
-            window.cordova.exec(success, failure, "FileChooser", "read_from_uri", [uri]);
-        },
-        writeToUri: function (success, failure, uri, data) {
-            window.cordova.exec(success, failure, "FileChooser", "write_to_uri", [uri, data]);
-        },
-        appendToUri: function (success, failure, uri, data) {
-            window.cordova.exec(success, failure, "FileChooser", "append_to_uri", [uri, data]);
-        }
-    };
-});
+    readFromUri: function (success, failure, uri) {
+        window.cordova.exec(success, failure, "FileStorage", "read_from_uri", [uri]);
+    },
+    writeToUri: function (success, failure, uri, data) {
+        window.cordova.exec(success, failure, "FileStorage", "write_to_uri", [uri, data]);
+    },
+    appendToUri: function (success, failure, uri, data) {
+        window.cordova.exec(success, failure, "FileStorage", "append_to_uri", [uri, data]);
+    }
+};
